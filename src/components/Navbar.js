e3f8acb7b95ac8ac85b1db170794c09c0472284d
Jane Doe <janedoe@example.com>
2026-04-10 15:54:47 +0530

    fix vercel build error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 function Navbar() {
-  const navigate = useNavigate();
+  
 
   const logout = () => {
     localStorage.removeItem("user_id");
